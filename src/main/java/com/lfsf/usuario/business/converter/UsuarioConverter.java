0b9d4967c65432e85f2afb3dfb970e3a52e8b58d
LuisFernandoSFonseca <luisf_sanches@live.com>
2026-01-28 22:04:56 -0300

    Adicionado o metodo para cadastro do usuário (Salva o usuário)

---
+++ src/main/java/com/lfsf/usuario/business/converter/UsuarioConverter.java
@@ -0,0 +1,105 @@
+package com.lfsf.usuario.business.converter;
+
+import com.lfsf.usuario.business.dto.EnderecoDTO;
+import com.lfsf.usuario.business.dto.TelefoneDTO;
+import com.lfsf.usuario.business.dto.UsuarioDTO;
+import com.lfsf.usuario.infrastructure.entity.Endereco;
+import com.lfsf.usuario.infrastructure.entity.Telefone;
+import com.lfsf.usuario.infrastructure.entity.Usuario;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UsuarioConverter {
+    public Usuario paraUsuario(UsuarioDTO usuarioDTO){
+        return Usuario.builder()
+                .nome(usuarioDTO.getNome())
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .endereco(paraListaEndereco(usuarioDTO.getEnderecos()))
+                .telefones(paraListaTelefones(usuarioDTO.getTelefones()))
+                .build();
+//        Usuario usuario= new Usuario();
+//        usuario.setEmail(usuarioDTO.getEmail());
+//        usuario.setNome(usuario.getNome());
+    }
+
+    public List<Endereco> paraListaEndereco (List<EnderecoDTO> enderecoDTOs){
+        //return enderecoDTOs.stream().map(this::paraEndereco).toList(); --> Java Stream, funciona como o For mas é menor e mais rápido
+        List<Endereco> enderecos = new ArrayList<>();
+        for(EnderecoDTO enderecoDTO: enderecoDTOs){
+            enderecos.add(paraEndereco(enderecoDTO));
+            return enderecos;
+        }
+        //Laço For
+    }
+
+    public Endereco paraEndereco(EnderecoDTO enderecoDTO){
+        return Endereco.builder()
+                .rua(enderecoDTO.getRua())
+                .numero(enderecoDTO.getNumero())
+                .cidade(enderecoDTO.getCidade())
+                .complemento(enderecoDTO.getComplemento())
+                .cep(enderecoDTO.getCep())
+                .estado(enderecoDTO.getEstado())
+                .build();
+    }
+
+    public List<Telefone> paraListaTelefones(List<TelefoneDTO> telefoneDTOS){
+        return telefoneDTOS.stream().map(this::paraTelefone).toList();
+    }
+
+    public Telefone paraTelefone(TelefoneDTO telefoneDTO){
+        return Telefone.builder()
+                .numero(telefoneDTO.getNumero())
+                .ddd(telefoneDTO.getDdd())
+                .build();
+    }
+
+    public UsuarioDTO paraUsuarioDTO(Usuario usuarioDTO){
+        return UsuarioDTO.builder()
+                .nome(usuarioDTO.getNome())
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .enderecos(paraListaEnderecoDTO(usuarioDTO.getEndereco()))
+                .telefones(paraListaTelefonesDTO(usuarioDTO.getTelefones()))
+                .build();
+//        Usuario usuario= new Usuario();
+//        usuario.setEmail(usuarioDTO.getEmail());
+//        usuario.setNome(usuario.getNome());
+    }
+
+    public List<EnderecoDTO> paraListaEnderecoDTO (List<Endereco> enderecoDTOs){
+        //return enderecoDTOs.stream().map(this::paraEndereco).toList(); --> Java Stream, funciona como o For mas é menor e mais rápido
+        List<EnderecoDTO> enderecos = new ArrayList<>();
+        for(Endereco enderecoDTO: enderecoDTOs){
+            enderecos.add(paraEnderecoDTO(enderecoDTO));
+            return enderecos;
+        }
+        //Laço For
+    }
+
+    public EnderecoDTO paraEnderecoDTO(Endereco enderecoDTO){
+        return EnderecoDTO.builder()
+                .rua(enderecoDTO.getRua())
+                .numero(enderecoDTO.getNumero())
+                .cidade(enderecoDTO.getCidade())
+                .complemento(enderecoDTO.getComplemento())
+                .cep(enderecoDTO.getCep())
+                .estado(enderecoDTO.getEstado())
+                .build();
+    }
+
+    public List<TelefoneDTO> paraListaTelefonesDTO(List<Telefone> telefoneDTOS){
+        return telefoneDTOS.stream().map(this::paraTelefoneDTO).toList();
+    }
+
+    public TelefoneDTO paraTelefoneDTO(Telefone telefoneDTO){
+        return TelefoneDTO.builder()
+                .numero(telefoneDTO.getNumero())
+                .ddd(telefoneDTO.getDdd())
+                .build();
+    }
+}
